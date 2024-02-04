3bd060128e07c0db6e3252fe162905337287cc2c
nipungupta99 <gupta.nipun99@gmail.com>
2024-02-04 08:35:57 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import youtubeThumbnail from "youtube-thumbnail";
+import { useState } from "react";
+import ReactPlayer from "react-player";
+export default function App() {
+  const [SelectedVideos, setSelectedVideos] = useState([]);
+  const data = [
+    "https://youtu.be/60tjWtmZ2AQ",
+    "https://youtu.be/sKkMXGFI854",
+    "https://youtu.be/UhFW3VreGJg",
+    "https://youtu.be/SXZGzp2IPlo",
+  ];
+
+  let thumbnailData = data.map((item) => {
+    return {
+      url: item,
+      thumbnail: youtubeThumbnail(item),
+    };
+  });
+
+  const updateSelection = (item) => {
+    // Check if the item is already selected
+    if (SelectedVideos.includes(item)) {
+      // Remove the item from the selection
+      setSelectedVideos(
+        SelectedVideos.filter((selectedItem) => selectedItem !== item)
+      );
+    } else {
+      // Add the item to the selection if less than 2 items are already selected
+      if (SelectedVideos.length < 2) {
+        setSelectedVideos([...SelectedVideos, item]);
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+        {thumbnailData.map((item) => {
+          return (
+            <button key={item.url} onClick={() => updateSelection(item.url)}>
+              {SelectedVideos.includes(item.url) && (
+                <span
+                  style={{
+                    background: "red",
+                    borderRadius: "50%",
+                    padding: "5px",
+                    position: "absolute",
+                  }}
+                >
+                  ✓
+                </span>
+              )}
+              <img src={item.thumbnail.default.url} />
+            </button>
+          );
+        })}
+      </div>
+      <div>
+        <div>
+          <h1>Selected Videos</h1>
+          <div
+            style={{
+              display: "flex",
+              gap: "10px",
+            }}
+          >
+            {SelectedVideos.map((item) => {
+              return (
+                <div>
+                  <ReactPlayer url={item} height="200px" width="300px" />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
