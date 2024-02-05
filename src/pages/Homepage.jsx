3038512444805ef23e987d5230b3344d2d89f7c2
NG <ng@ng.com>
2024-02-05 21:43:37 +0530

    Added changes in selection page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,19 +9,41 @@ const Homepage = () => {
     {
       monthName: "June",
       videos: [
-        "https://youtu.be/60tjWtmZ2AQ",
-        "https://youtu.be/sKkMXGFI854",
-        "https://youtu.be/UhFW3VreGJg",
-        "https://youtu.be/SXZGzp2IPlo",
+        {
+          name: "https://youtu.be/60tjWtmZ2AQ",
+          url: "https://youtu.be/60tjWtmZ2AQ",
+        },
+        {
+          name: "https://youtu.be/sKkMXGFI854",
+          url: "https://youtu.be/sKkMXGFI854",
+        }, {
+          name: "https://youtu.be/UhFW3VreGJg",
+          url: "https://youtu.be/UhFW3VreGJg",
+        },
+        {
+          name: "https://youtu.be/SXZGzp2IPlo",
+          url: "https://youtu.be/SXZGzp2IPlo",
+        },
       ],
     },
     {
       monthName: "July",
       videos: [
-        "https://youtu.be/60tjWtmZ2AQ",
-        "https://youtu.be/sKkMXGFI854",
-        "https://youtu.be/UhFW3VreGJg",
-        "https://youtu.be/SXZGzp2IPlo",
+        {
+          name: "https://youtu.be/60tjWtmZ2AQ",
+          url: "https://youtu.be/60tjWtmZ2AQ",
+        },
+        {
+          name: "https://youtu.be/sKkMXGFI854",
+          url: "https://youtu.be/sKkMXGFI854",
+        }, {
+          name: "https://youtu.be/UhFW3VreGJg",
+          url: "https://youtu.be/UhFW3VreGJg",
+        },
+        {
+          name: "https://youtu.be/SXZGzp2IPlo",
+          url: "https://youtu.be/SXZGzp2IPlo",
+        },
       ],
     },
   ];
@@ -38,12 +60,14 @@ const Homepage = () => {
       return {
         id,
         monthName: month.monthName,
-        url: videoUrl,
-        thumbnail: youtubeThumbnail(videoUrl).default.url,
+        url: videoUrl.url,
+        name: videoUrl.name,
+        thumbnail: youtubeThumbnail(videoUrl.url).default.url,
       };
     }),
   }));
 
+  console.log(thumbnailData, "THumbnail data")
   const showPreview = () => {
     alert("hello");
   };
@@ -93,43 +117,43 @@ const Homepage = () => {
 
   return (
     <div>
-      <h1>Homepage</h1>
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <div className="flex justify-center items-center py-5" >
+        <h1 className="flex text-[34px] font-bold" >Select Videos</h1>
+      </div>
+      <div className="grid grid-cols-2">
         {thumbnailData.map((item) => (
-          <div key={item.monthName} style={{ width: "45vw", margin: "10px" }}>
-            <h3>{item.monthName}</h3>
-            <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+          <div key={item.monthName} className="p-4">
+            <h3 className="mb-2" >Month: {item.monthName}</h3>
+            <div className="flex flex-col wrap ">
               {item.data.map((ele) => (
                 <div
                   key={ele.id}
                   onClick={() => handleSelectVideo(ele.id, ele.monthName)}
+                  className="flex p-2 w-full border-y gap-3 items-center hover:bg-[#CFCBC0]"
                 >
-                  {selectedVideos.some((video) => video.id === ele.id) && (
+
                     <div
-                      style={{
-                        position: "absolute",
-                        backgroundColor: "green",
-                        padding: "5px",
-                        color: "white",
-                      }}
+                        className=" border h-[20px] w-[20px] text-white"
                     >
-                      <FaCheck />
+                      {selectedVideos.some((video) => video.id === ele.id) && (  <FaCheck color={"blue"} />)}
                     </div>
-                  )}
+
                   <img
                     src={ele.thumbnail}
-                    height="200"
-                    width="300"
+                    height="30"
+                    width="60"
                     alt="video thumbnail"
                   />
+                  <p>{ele.name}</p>
                 </div>
               ))}
             </div>
           </div>
         ))}
       </div>
-      <div style={{ display: "flex", justifyContent: "center" }}>
+      <div className="flex justify-center" >
         <button
+          className="border px-4 py-3 bg-[darkblue] text-white font-bold rounded mt-10 hover:bg-[#001F63]"
           onClick={() => {
             navigate("/player", { state: selectedVideos });
           }}
@@ -137,7 +161,6 @@ const Homepage = () => {
           Go to comparison
         </button>
       </div>
-      {JSON.stringify(selectedVideos)}
     </div>
   );
 };
